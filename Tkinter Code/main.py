d5163cb163895b8a76ea96d1055e3e29ce4dba40
dannyjriggleman09 <dannyjriggleman09@gmail.com>
2025-03-21 10:59:25 +0000

    Progress Update

--- Tkinter Code/main.py
+++ Tkinter Code/main.py
@@ -1,5 +1,17 @@
 from tkinter import *
 
+class MyApp:
+
+    def __init__(self, root):
+        
+        root.title("My app")
+        root.geometry("500x400")
+        root.maxsize(1000,800)
+
+        label = Label(root, text="Some label text")
+        label.pack()
+
+
 root = TK()
-root.title("My app")
+MyApp(root)
 root.mainloop()
