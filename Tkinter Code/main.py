25808d1ef45fffa878f7b03006060781822a094d
dannyjriggleman09 <dannyjriggleman09@gmail.com>
2025-03-21 16:10:13 +0000

    Continued Progress w/ Tkinter

--- Tkinter Code/main.py
+++ Tkinter Code/main.py
@@ -1,17 +1,18 @@
 from tkinter import *
+import tkinter as tk # This lets us shorten tkinter to tk in method calls.
 
 class MyApp:
 
     def __init__(self, root):
         
         root.title("My app")
-        root.geometry("500x400")
-        root.maxsize(1000,800)
+        # root.geometry("500x400")
+        # root.maxsize(1000,800)
 
         self.label_text = StringVar()
         label = Label(root, text="Some label text", textvariable=self.label_text)
-        label.pack()
-
+        # label.pack(side=tk.LEFT, padx=40, pady=20)
+        label.grid(column=1, row=1)
         # label["text"] = "New label text"
         # label["font"] = ("Courier", 40)
 
@@ -20,19 +21,23 @@ class MyApp:
         # entry_text = StringVar()
         self.entry_text = StringVar()
         entry = Entry(root, textvariable=self.entry_text)
-        entry.pack() # Is placed below the label
+        # entry.pack(side=tk.LEFT) # Is placed below the label
+        # entry.place(x=100, y=50)
+        entry.grid(column = 2, row=1)
 
         # label["textvariable"] = entry_text
 
         # Create a Button
         button = Button(root, text="Button text", command=press_button)
-        button.pack()
+        # button.pack(side=tk.LEFT)
+        button.grid(column=1, row=2)
 
         # Create Listbox
         self.list_item_strings = ["Hey", "Hi", "Hello", "Howdy", "Greetings"]
         list_items = StringVar(value=self.list_item_strings)
         self.listbox = Listbox(root, listvariable=list_items)
-        listbox.pack()
+        # listbox.pack(side=tk.BOTTOM)
+        listbox.grid(column=2,row=2)
         listbox["height"] = 3
         listbox.bind("<<ListboxSelect>>", lambda s: self.select_item(listbox.curselection()))
 
