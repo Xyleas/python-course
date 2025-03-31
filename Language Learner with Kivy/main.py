42659bb767255014098897fb36b5516fc8095827
dannyjriggleman09 <dannyjriggleman09@gmail.com>
2025-03-31 15:03:45 +0000

    https://keep.google.com/u/0/#NOTE/1tTiUGuZLcbvDtgFmi6uQhOwbcep44MP-2vAqu8MwEnNAvY0oJheteIk1xAErbg

--- Language Learner with Kivy/main.py
+++ Language Learner with Kivy/main.py
@@ -1,12 +1,25 @@
 from kivy.app import app
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.properties import StringProperty, NumericProperty
+from kivy.core.window import Window
+
+Window.clearcolor = (1,1,1,1)
+Window.size = (400,600)
+
+class ProgressBar(Widget):
+    pass
+
+class ImageBox(Widget):
+    pass
 
 class GameScreen(Widget):
     pass
 
 class LanguageLearnerApp(App):
     def build(self):
+        game_screen = GameScreen()
         return GameScreen()
 
 if __name__ == '__main__':
