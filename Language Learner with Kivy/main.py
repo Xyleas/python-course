b57368870f83ecc933f557db6429928cbc2d8b7a
dannyjriggleman09 <dannyjriggleman09@gmail.com>
2025-03-31 21:09:31 +0000

    https://keep.google.com/u/0/#NOTE/1tTiUGuZLcbvDtgFmi6uQhOwbcep44MP-2vAqu8MwEnNAvY0oJheteIk1xAErbg

--- Language Learner with Kivy/main.py
+++ Language Learner with Kivy/main.py
@@ -2,20 +2,93 @@ from kivy.app import app
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, ListProperty
 from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.image import Image
+from.kivy.uix.modalview import ModalView
+
+from random import randint
 
 Window.clearcolor = (1,1,1,1)
 Window.size = (400,600)
+
+class EndGameScreen(ModalView):
+    pass
 
 class ProgressBar(Widget):
     pass
 
 class ImageBox(Widget):
-    pass
+    index = NumericProperty(0)
+    image_name = StringProperty("")
+    def get_image_path(self, image_name):
+        return "images/" + image_name + ".png"
 
 class GameScreen(Widget):
-    pass
+    correct_answer_index = NumericProperty(0)
+    display_image_names = ListProperty([
+        "bote",
+        "diamante",
+        "espada",
+        "hacha"
+    ])
+    correct_answer_text = StringProperty("bote")
+    item_names = [
+        "bote",
+        "diamante",
+        "espada",
+        "hacha",
+        "ladrillos",
+        "manzana",
+        "pala",
+        "pastro",
+        "roca",
+        "tierra"
+    ]
+
+    def __init__(self, **kwargs):
+        super(GameScreen, self).__init__(**kwargs)
+        self.load_new_questions()
+
+    def get_random_image_names(self):
+        temp_item_names = self.item_names.copy()
+        items = []
+        for _ in range(4):
+            rand_index = randint(0, len(temp_item_names)-1)
+            items.append(temp_item_names[rand_index])
+            del temp_item_names[rand_index]
+        return items
+
+    def load_new_questions(self):
+        self.display_image_names = self.get_random_image_names()
+        self.correct_answer_index = randint(0,3)
+        self.correct_answer_text = self.display_image_names[rand_index]
+        # Choose 4 random images
+        # Randomly choose correct answer
+        # Display images and label
+
+    def show_end_game_popup(self):
+        end_game_screen = EndGameScreen(size_hint(.75,.6), auto_dismiss=False)
+        end_game_screen.open()
+
+    def show_answer_popup(self):
+        content = Image(source='images/bote.png')
+
+        popup = Popup(
+            title = 'Correct',
+            size_hint=(.5,.4),
+            content= self.content,
+            title_align = 'center',
+            title_color = (0,0,0,1),
+            background = 'images/white.png'
+        )
+        popup.bind(on_dismiss=self.print_something)
+        popup.open()
+
+
+    def print_something( self, *args):
+        print("popup dismissed")
 
 class LanguageLearnerApp(App):
     def build(self):
