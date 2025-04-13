76f659e5b5e1889a292d5409ce3143aac2afa568
dannyjriggleman09 <dannyjriggleman09@gmail.com>
2025-04-13 19:18:05 +0000

    🔢 OpenCV For Beginners https://keep.google.com/u/0/#NOTE/1qmYUY-REbhPAu3PeC3nTyYn4SsUfKQcXdPoMYwogwb4L3s7eLvUDYvEQpEJ30A

---
+++ 🔢 OpenCV For Beginners/Feature_Detection_and_Matching.py
@@ -0,0 +1,46 @@
+import cv2
+
+def display_image(image):
+    cv2.imshow("Grey Scale Images", image)
+    cv2.waitKey(0)
+
+def feature_detection_and_matching(image1_path, image2_path):
+
+    # Load the image
+    image1 = cv2.imread(image1_path)
+    image2 = cv2.imread(image2_path)
+
+    # Convert to greyscale
+    grey1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GREY)
+    grey2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GREY)
+
+    # display_image(grey1)
+
+    # Initialize SIFT detector
+    sift = cv2.SIFT_create()
+
+    # Detect keypoints and descriptors
+    keypoints1, descriptors1, = sift.detectAndCompute(grey1, None)
+    keypoints2, descriptors2, = sift.detectAndCompute(grey2, None)
+
+    # Use BFMatcher to match descriptors
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+
+    # Apply Lowe's ratio test to filter out weak matches
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
+
+    # Draw matches
+    matched_image = cv2.drawMatchesKnn(image1, keypoints1, image2, keypoints2, good_matches, None, flags=2)
+
+    return matched_image
+
+image1_path = "images/tea-cup.jpeg"
+image2_path = "images/tea-cup-2.jpeg" 
+
+matched_image = feature_detection_and_matching(image1_path, image2_path)
+
+display_image(matched_image)
--- 🔢 OpenCV For Beginners/Image_Filtering_and_Enhancement.py
+++ 🔢 OpenCV For Beginners/Image_Filtering_and_Enhancement.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def display_image(image):
     cv2.imshow("Image", image)
@@ -8,6 +9,17 @@ def apply_blur(image, kernel_size):
     blurred_image = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
 
     return blurred_image
+
+def apply_sharpening(image):
+
+    sharpening_kernel = np.array([[-1, -1, -1],
+                                [-1, 9, -1],
+                                [-1, -1, -1]])
+
+    sharpened_image = cv2.filter2D(image, -1, sharpening_kernel)
+
+    return sharpened_image
+
 def main():
     # Referenbce to the path of our image
     image_path = "images/tea-cup.jpeg"
@@ -19,7 +31,15 @@ def main():
 
     blurred_image = apply_blur(image, kernel_size)
 
+    sharpened_image = apply_sharpening(image)
+    # Display original image
+    display_image(image)
+
+    # Display blurred image
     display_image(blurred_image)
+
+    # Display sharpened image
+    display_image(sharpened_image)
 
 # Call our main function
 main()
