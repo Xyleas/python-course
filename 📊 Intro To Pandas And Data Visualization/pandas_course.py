3a98e8cb87b671cfd1d0b44699813e6c3a14d7d9
dannyjriggleman09 <dannyjriggleman09@gmail.com>
2025-04-26 10:51:30 +0000

    📊 Intro To Pandas And Data Visualization https://keep.google.com/u/0/#NOTE/1v7WPl3HbJM1tcwhpaGtx4YAWJSeHHUdVM3EYtLvit1kEVvgVKnIVIoY56sLvFkI

--- 📊 Intro To Pandas And Data Visualization/pandas_course.py
+++ 📊 Intro To Pandas And Data Visualization/pandas_course.py
@@ -67,3 +67,75 @@ print(df_dropped_columns)
 
 df_filled = df.fillna(0)
 print(df_filled)
+
+df = pd.concat([df, df.iloc[2]])
+print(df)
+
+df_no_duplicates = df.drop_duplicates
+print(df_no_duplicates)
+
+# map()
+def categorize(value):
+    if pd.isna(value):
+        return 'Missing'
+    elif value < 3:
+        return 'Low'
+    else:
+        return 'High'
+df['B'] = df['B'].map(categorize)
+print(df)
+
+data = {
+    'School' : ['A', 'B', 'A', 'B', 'A', 'B', 'A', 'B'],
+    'Student ID' : [1001,1002,1003,1004,1005,1006,1007],
+    'Math' : np.random.randint(60,100,8),
+    'English' : np.random.randint(70,100,8),
+    'History' : np.random.randint(50,100,8)
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+grouped = df.groupby('School')
+print(grouped)
+#print('Mean scores by School: ')
+#print(grouped.mean())
+
+agg_data = grouped.agg({
+    'Math': ['mean', 'median', 'std']
+})
+
+# print(agg_data)
+
+agg_data_multi = grouped.agg({
+    'Math': ['mean', 'median', 'min'],
+    'English': ['mean', 'max'],
+    'History': ['min', 'max']
+})
+
+# print(agg_data_multi)
+df['Class'] = ['X', 'X', 'Y', 'Y', 'X', 'Y', 'Y', 'X']
+grouped_multi = df.groupby(['School', 'Class'])
+print(grouped_multi.mean())
+
+# CHALLENGE
+# Use grouping and aggregating to calculate the median score across all classes by school.
+
+# AI Output
+grouped_median = df.groupby('School').agg({'Math': 'median', 'English': 'median', 'History': 'median'})
+print(grouped_median)
+
+# Human Output
+print(df.groupby(['School', 'Class']).median())
+
+sports_data = {
+    'Student ID': [1001, 1002, 1003, 1004, 1009, 1010],
+    'Sports': ['Basketball', 'Football', 'Badminton', 'Basketball', 'Football', 'Tennis']
+}
+
+sports_df = pd.DataFrame(sports_data)
+
+merged_df = pd.merge(df, sports_df, on='Student ID') # Defaults to Inner Join Only Join Rows where BOTH have values. 1009 and 1010 do not appear
+
+print(merged_df)
+
